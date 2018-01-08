02488d7cfd416dbf8c1be09fb486f754ea40f542
BIT-USER <BIT-USER@DESKTOP-6G8H6K0>
2018-01-08 16:52:55 +0900

    1st

---
+++ src/com/javaex/problem02/FriendApp.java
@@ -0,0 +1,10 @@
+package com.javaex.problem02;
+
+public class FriendApp {
+
+    public static void main(String[] args) {
+
+       
+    }
+
+}
---
+++ src/com/javaex/problem03/GoodsApp.java
@@ -0,0 +1,14 @@
+package com.javaex.problem03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GoodsApp {
+
+    public static void main(String[] args) {
+
+       
+    }
+
+}
